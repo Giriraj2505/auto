3414b31bf81b762762c0beda9347d40c08581942
Giriraj2505 <98442107+Giriraj2505@users.noreply.github.com>
2022-09-12 07:37:33 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -8,7 +8,7 @@ from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
 
-"""-----------------------------------------https://t.me/htdupdate --------------------------------------"""
+"""-----------------------------------------http://t.me/Hindi_Request_Movies --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/htdupdate')
+            InlineKeyboardButton('📢 Updates', url='http://t.me/Hindi_Request_Movies')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
